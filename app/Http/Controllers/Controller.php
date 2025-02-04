970eff8b80de39cdeae704b20954b1714172c4c5
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-04 01:31:25 -0600

    feat(Response): :sparkles: First custom response

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    public function response(int $status, bool $error, string $message = '', array|object $data = []): object
+    {
+        return response()->json([
+            'status' => $status,
+            'error' => $error,
+            'message' => $message,
+            'data' => $data
+        ], $status);
+    }
+}
