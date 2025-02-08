c20df3a2d07ba046ac34b8b787623dcbe2027181
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-07 23:28:13 -0600

    feat(Config): :heavy_plus_sign: Add swagger dependency

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,19 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
+    /**
+ * @OA\Info(
+ *    title="Laravel IAM Swagger API",
+ *    version="1.0.0",
+ * )
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="bearerAuth",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ *
+ */
     public function response(int $status, bool $error, string $message = '', array|object $data = []): object
     {
         return response()->json([
