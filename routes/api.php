9f51d3be56d0879d9437e934bedcd4203f5ea5c7
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-09 05:20:32 -0600

    feat(API): :sparkles: Use custom rate limits

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,31 @@ use App\Http\Controllers\Api\profileController;
 use App\Http\Controllers\Api\userController;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\CheckPermissions;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
-// crear un routeGroup para los modulos /modules
+// rate limits
 
-Route::middleware(['auth:sanctum'])->group(function () {
+RateLimiter::for('login', function (Request $request) {
+    return Limit::perMinute(5)->by($request->ip()); // Máximo 5 intentos por IP en 1 minuto
+});
+
+RateLimiter::for('user_actions', function (Request $request) {
+    return Limit::perMinute(10)->by($request->ip()); // Máximo 60 peticiones por IP en 1 minuto
+});
+
+// Routes
+Route::post('/auth/login', [AuthController::class, 'login'])->middleware('throttle:login');
+
+Route::middleware(['auth:sanctum', 'throttle:user_actions'])->group(function () {
+
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/auth/permissions', 'permissions');
+        Route::post('/auth/logout', 'logout');
+    });
+
     Route::controller(moduleController::class)->group(function () {
         Route::get('/modules', 'all')->middleware(CheckPermissions::class . ':modules,show');
         Route::get('/modules/{id}', 'show')->middleware(CheckPermissions::class . ':modules,show');
@@ -46,19 +66,14 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::controller(userController::class)->group(function () {
         Route::get('/users', 'all')->middleware(CheckPermissions::class . ':users,show');
-        Route::get('/users/{id}', 'show')->middleware(CheckPermissions::class . 'users,show');
-        Route::post('/users', 'store')->middleware(CheckPermissions::class . ':profiles,create');
-        Route::put('/users/{id}', 'update')->middleware(CheckPermissions::class . 'users,update');
-        Route::patch('users/{id}/status', 'updateStatus')->middleware(CheckPermissions::class . 'users');
-    });
+        Route::get('/users/{id}', 'show')->middleware(CheckPermissions::class . ':users,show');
+        Route::post('/users', 'store')->middleware(CheckPermissions::class . ':users,create');
+        Route::put('/users/{id}', 'update')->middleware(CheckPermissions::class . ':users,update');
+        Route::patch('users/{id}/status', 'updateStatus')->middleware(CheckPermissions::class . ':users,updateStatus');
+        Route::patch('users/{id}/password', 'updateStatus')->middleware(CheckPermissions::class . ':users,updatePassword');
 
-    Route::controller(AuthController::class)->group(function () {
-        Route::get('/auth/permissions', 'permissions');
-        Route::post('/auth/logout', 'logout');
     });
 });
-
-Route::post('/auth/login', [AuthController::class, 'login']);
 
 // // definir ruta default en caso de que no exista
 Route::fallback(function () {
